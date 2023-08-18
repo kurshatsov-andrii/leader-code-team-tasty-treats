9f0dc394da5b0bceb4fce075be0532798724f152
BogdanK85 <kobakbogdan@gmail.com>
2023-08-18 19:56:03 +0300

    first version

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // import './js/header.js';
 // import './js/hero.js';
 // import './js/all-categories.js';
-// import './js/popular-recipe.js';
+import './js/popular-recipes.js';
 // import './js/catalog.js';
 // import './js/favorites.js';
 // import './js/modal-window-recipe.js';
