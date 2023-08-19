cab53f2cbd68a92685518e559e468f07dc3277a7
RuslanMirasov <info@mirasov.dev>
2023-08-20 01:27:17 +0200

    Order send + theme switcher (JS-part)

--- src/js/theme-switcher.js
+++ src/js/theme-switcher.js
@@ -4,12 +4,14 @@ if (localStorage.theme !== undefined && localStorage.theme !== 'light') {
   body.classList.add('dark-theme');
 }
 
-switcher.addEventListener('click', () => {
-  if (body.classList.contains('dark-theme')) {
-    body.classList.remove('dark-theme');
-    localStorage.theme = 'light';
-    return;
-  }
-  body.classList.add('dark-theme');
-  localStorage.theme = 'dark';
-});
+if (switcher) {
+  switcher.addEventListener('click', () => {
+    if (body.classList.contains('dark-theme')) {
+      body.classList.remove('dark-theme');
+      localStorage.theme = 'light';
+      return;
+    }
+    body.classList.add('dark-theme');
+    localStorage.theme = 'dark';
+  });
+}
