fc7a7eecc1d6caa1641721c316b941ef24f9287a
BogdanK85 <kobakbogdan@gmail.com>
2023-08-20 20:39:05 +0300

    add id

--- src/js/popular-recipes.js
+++ src/js/popular-recipes.js
@@ -41,10 +41,10 @@ if (popularRecipes) {
 function createPopularRecipesMarkup(recipes) {
     return recipes.map(({ title, description, preview, _id }) => {
         return `
-        <li class="popular-recipes-list list" data-id=${_id}>
+        <li class="popular-recipes-list list" >
             <a class="popular-recipes-link" href="#!" >
                 <div class="popular-recipes-item">
-                  <img class="popular-recipes-img" src="${preview}" alt="${title}">
+                  <img class="popular-recipes-img" src="${preview}" alt="${title}" data-id=${_id}>
                   <div class="popular-recipes-wraper">
                     <h3 class="popular-recipes-title">${title}</h3>
                     <p class="popular-recipes-description">${description}</p>
