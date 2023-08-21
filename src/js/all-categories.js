75dd4f2bfeb6aef65291bcfdad800331d068f261
IrynaSymon <126278348+IrynaSymon@users.noreply.github.com>
2023-08-21 17:52:28 +0200

    ok

--- src/js/all-categories.js
+++ src/js/all-categories.js
@@ -1,13 +1,15 @@
 import axios from "axios"
 import { Notify } from "notiflix";
 import { createAllRecipesMarkUp } from "./catalog"
-
+const active = document.querySelector('.active');
 const categoriesAll = document.querySelector('.categories-wrapper');
 const btnCategory = document.querySelector(".btn");
 const recipeList = document.querySelector('.recipe-list');
-
+const allCategories = document.querySelector('.all-categories');
+//------
 const BASE_URL = 'https://tasty-treats-backend.p.goit.global/api/';
-//-----
+
+//------
 async function getFetchCategories() {
   try {
       const response = await axios.get(`${BASE_URL}categories`);
@@ -31,9 +33,9 @@ if (categoriesAll) {
 }
 //------
 function createAllCategoriesMarkup(categories) {
-    return categories.map(({ id, name }) => {
+    return categories.map(({ _id, name }) => {
         return `
-        <li class="categories-item" data-id=${id}>
+        <li class="categories-item" data-id=${_id}>
         <button class="btn">${name}</button>
         </li>`
     }).join("");  
@@ -44,12 +46,15 @@ function createAllCategoriesMarkup(categories) {
 if (categoriesAll) {
   categoriesAll.addEventListener('click', handleCategoriesListClick);
 }
+
+
 function handleCategoriesListClick(event) {
   if (!event.target.classList.contains('btn')) {
     return;
   }
   console.log(event.target.innerText);
   let pickedCategory = event.target.innerText;
+  event.target.classList.add("active")
   renderRecipe(pickedCategory);
 }
 
@@ -72,3 +77,17 @@ async function renderRecipe(category) {
     Notiflix.Notify.failure('Ooops! No recipes found');
   }
 }
+// _____
+const handleClick = () => {
+  console.log("click event listener callback");
+};
+
+addListenerBtn.addEventListener("click", () => {
+  btn.addEventListener("click", handleClick);
+  console.log("click event listener was added to btn");
+});
+
+removeListenerBtn.addEventListener("click", () => {
+  btn.removeEventListener("click", handleClick);
+  console.log("click event listener was removed from btn");
+});
