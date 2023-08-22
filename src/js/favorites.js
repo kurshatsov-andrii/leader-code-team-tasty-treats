df91809ddb2bbd78d6fc2a6113af13d57abffec8
Andrii Kurshatsov <andreswebit@ukr.net>
2023-08-22 14:47:30 +0300

    Update favorites.js

--- src/js/favorites.js
+++ src/js/favorites.js
@@ -19,6 +19,7 @@ export class FechFullRecipe {
 import _ from 'lodash';
 import Notiflix from 'notiflix';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { recipeList } from '../js/catalog';
 
 const recipeList = document.querySelector('.recipe-list');
 const searchInput = document.querySelector('.search-input');
