b3b79c672ee3aa5238758b6e08835d12b73c7360
RuslanMirasov <info@mirasov.dev>
2023-08-22 13:35:56 +0200

    Youtube in recepie

--- src/js/modal-window-recipe.js
+++ src/js/modal-window-recipe.js
@@ -1,20 +1,13 @@
 import Notiflix from 'notiflix';
-
-import axios from "axios";
-
 import { openPopupById } from '../js/custom-popup';
 import { closeOllPopups } from '../js/custom-popup';
 
-
-
-
-const recipeModalWindow = document.querySelector(".backend-info");
+const recipeModalWindow = document.querySelector('.js-backend-info');
 const popularRecipeList = document.querySelector('.popular-recipes-js');
 const addFavoriteBtn = document.querySelector('.favorite-btn');
 const recipeList = document.querySelector('.recipe-list');
 const favoriteArr = [];
 const KEY_FAVORITE = 'favorite';
-
 
 if (popularRecipeList) {
   popularRecipeList.addEventListener('click', onPopularRecipe);
@@ -27,79 +20,76 @@ if (addFavoriteBtn) {
   addFavoriteBtn.addEventListener('click', onFavoriteBtn);
 }
 
-
-
-
-let recipeId = "";
+let recipeId = '';
 let recipes = {};
 
 function onCatalogRecipe(evt) {
   evt.preventDefault();
   if (evt.target.nodeName !== 'BUTTON') {
-    return
+    return;
   }
   recipeId = evt.target.dataset.id;
 
   // document.addEventListener('keydown', onEscClose);
-  
+
   openPopupById('recepie');
   fetchRecipes(recipeId);
-  renderModalWindow(recipeId)
-
+  renderModalWindow(recipeId);
 }
 
 function onPopularRecipe(evt) {
   evt.preventDefault();
   if (evt.target.nodeName !== 'IMG') {
-    return
+    return;
   }
   recipeId = evt.target.dataset.id;
-
-  // document.addEventListener('keydown', onEscClose);
-  
-  openPopupById('recepie');
   fetchRecipes(recipeId);
-  renderModalWindow(recipeId)
-
+  renderModalWindow(recipeId);
 }
 
 async function fetchRecipes(id) {
-    const response = await fetch(
-      `https://tasty-treats-backend.p.goit.global/api/recipes/${id}`
-    );
-  
-    return response;
-    
-  }
+  const response = await fetch(`https://tasty-treats-backend.p.goit.global/api/recipes/${id}`);
 
-  async function renderModalWindow(id) {
-    try {
+  return response;
+}
+
+async function renderModalWindow(id) {
+  openPopupById('loading');
+  try {
     const response = await fetchRecipes(recipeId);
     recipes = await response.json();
-    
     recipeModalWindow.innerHTML = modalWindowRecipesMarkUp(recipes);
-}
-catch(err) {Notiflix.Report.failure(err); console.log(err)}
+    setTimeout(() => {
+      openPopupById('recepie');
+    }, 350);
+  } catch (err) {
+    setTimeout(() => {
+      closeOllPopups();
+      Notiflix.Report.failure(err);
+    }, 350);
   }
+}
 
 function modalWindowRecipesMarkUp(obj) {
-const { title, instructions, youtube, ingredients, rating, tags, time, preview } = obj;
-const addHtml = `<h2>${title.toUpperCase()}</h2>
+  const { title, instructions, youtube, ingredients, rating, tags, time, preview } = obj;
+  console.log(youtube);
+  const addHtml = `
+      <h2 class="js-recepie-popup-title">${title.toUpperCase()}</h2>
       <div class="video-recipe">
-      <video class="video-cont"
-  src=${youtube}
-  poster=${preview}
-  
-  controls
-  autoplay
-  loop
-  preload="auto"
-></video>
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/${youtube.split('?v=')[1]}"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowfullscreen
+        ></iframe>
       </div>
       <div class="info-panel">
         <div class="tags">
           <ul class="tags-list">
-          ${tags.map((tag) => `<li><span class="tag-item"><p class="modal-text">#${tag}</p></span></li>`).join("")}
+          ${tags.map(tag => `<li><span class="tag-item"><p class="modal-text">#${tag}</p></span></li>`).join('')}
           </ul>
         </div>
         
@@ -124,33 +114,26 @@ const addHtml = `<h2>${title.toUpperCase()}</h2>
       </div>
       <div class="ingredients">
         <ul class="ingredients-list">
-        ${ingredients.map(({name, measure} = ingredients) => `<li class="ingredients-item"><div><p class="modal-text">${name}</p></div><div><pclass="modal-text">${measure}</pclass=></div></li>`).join("")}
+        ${ingredients
+          .map(
+            ({ name, measure } = ingredients) =>
+              `<li class="ingredients-item"><div><p class="modal-text">${name}</p></div><div><pclass="modal-text">${measure}</pclass=></div></li>`
+          )
+          .join('')}
         </ul>
       </div>
       <div class="instructions-container">
       <p class="instructions-text">${instructions}</p></div>
       <button type="button" class="favorite-btn btn">Add to favorite</button>
-      `
-      
-      return addHtml;
-      
-    
+      `;
+
+  return addHtml;
 }
 
 function onFavoriteBtn(evt) {
-
   evt.preventDefault();
 
   favoriteArr.push(recipes);
   localStorage.setItem(KEY_FAVORITE, JSON.stringify(favoriteArr));
   console.log(recipes);
-
-  
 }
-
-
-
-
-
-
-
