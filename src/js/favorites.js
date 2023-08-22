931c45626c3bf3b766822253df21eef16460ad61
Andrii Kurshatsov <andreswebit@ukr.net>
2023-08-22 15:12:10 +0300

    Update favorites.js

--- src/js/favorites.js
+++ src/js/favorites.js
@@ -1,234 +1,234 @@
-import axios from 'axios';
+// import axios from 'axios';
 
-export class FechFullRecipe {
-  #BASE_URL = `https://tasty-treats-backend.p.goit.global/api/recipes`;
-  constructor(ID) {
-    this.ID = ID;
-  }
+// export class FechFullRecipe {
+//   #BASE_URL = `https://tasty-treats-backend.p.goit.global/api/recipes`;
+//   constructor(ID) {
+//     this.ID = ID;
+//   }
 
-  async getRecipe() {
-    try {
-      const recipe = await axios.get(`${this.#BASE_URL}/${this.ID}`);
-      return recipe;
-    } catch (error) {
-      console.log(error);
-    }
-  }
-}
+//   async getRecipe() {
+//     try {
+//       const recipe = await axios.get(`${this.#BASE_URL}/${this.ID}`);
+//       return recipe;
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   }
+// }
 
-import _ from 'lodash';
-import Notiflix from 'notiflix';
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
+// import _ from 'lodash';
+// import Notiflix from 'notiflix';
+// import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import { recipeList } from '../js/catalog';
 
-//const recipeList = document.querySelector('.recipe-list');
-const searchInput = document.querySelector('.search-input');
-const reset = document.querySelector('.reset-wrap');
-const allCategories = document.querySelector('.all-categories');
+// //const recipeList = document.querySelector('.recipe-list');
+// const searchInput = document.querySelector('.search-input');
+// const reset = document.querySelector('.reset-wrap');
+// const allCategories = document.querySelector('.all-categories');
 
-const categoriesList = document.querySelector('.categories-wrapper');
+// const categoriesList = document.querySelector('.categories-wrapper');
 
-// ==================ЗМІННІ================
-const BASE_URL = 'https://tasty-treats-backend.p.goit.global/api/';
+// // ==================ЗМІННІ================
+// const BASE_URL = 'https://tasty-treats-backend.p.goit.global/api/';
 
-export let page = 1;
+// export let page = 1;
 
-let PER_PAGE = 0;
-if (document.documentElement.clientWidth < 768) {
-  PER_PAGE = 6;
-} else if (document.documentElement.clientWidth >= 768 && document.documentElement.clientWidth < 1280) {
-  PER_PAGE = 8;
-} else {
-  PER_PAGE = 9;
-}
+// let PER_PAGE = 0;
+// if (document.documentElement.clientWidth < 768) {
+//   PER_PAGE = 6;
+// } else if (document.documentElement.clientWidth >= 768 && document.documentElement.clientWidth < 1280) {
+//   PER_PAGE = 8;
+// } else {
+//   PER_PAGE = 9;
+// }
 
-export let totalPages = 0;
-if (document.documentElement.clientWidth < 768) {
-  totalPages = 48;
-} else if (document.documentElement.clientWidth >= 768 && document.documentElement.clientWidth < 1280) {
-  totalPages = 36;
-} else {
-  totalPages = 32;
-}
+// export let totalPages = 0;
+// if (document.documentElement.clientWidth < 768) {
+//   totalPages = 48;
+// } else if (document.documentElement.clientWidth >= 768 && document.documentElement.clientWidth < 1280) {
+//   totalPages = 36;
+// } else {
+//   totalPages = 32;
+// }
 
-// ================ПРОСЛУХОВУВАЧІ ПОДІЙ=================
+// // ================ПРОСЛУХОВУВАЧІ ПОДІЙ=================
 
-if (searchInput) {
-  searchInput.addEventListener('input', _.debounce(handleSearchInput, 500));
-}
+// if (searchInput) {
+//   searchInput.addEventListener('input', _.debounce(handleSearchInput, 500));
+// }
 
-function handleSearchInput(event) {
-  const searchedTitle = event.target.value.trim();
-  renderSearchedRecipes(searchedTitle);
-}
+// function handleSearchInput(event) {
+//   const searchedTitle = event.target.value.trim();
+//   renderSearchedRecipes(searchedTitle);
+// }
 
-if (reset) {
-  reset.addEventListener('click', handleResetClick);
-}
+// if (reset) {
+//   reset.addEventListener('click', handleResetClick);
+// }
 
-function handleResetClick() {
-  searchInput.value = '';
-  renderAllRecipes();
-}
+// function handleResetClick() {
+//   searchInput.value = '';
+//   renderAllRecipes();
+// }
 
-if (allCategories) {
-  allCategories.addEventListener('click', handleAllCategoriesClick);
-}
+// if (allCategories) {
+//   allCategories.addEventListener('click', handleAllCategoriesClick);
+// }
 
-function handleAllCategoriesClick() {
-  searchInput.value = '';
-  renderAllRecipes();
-}
+// function handleAllCategoriesClick() {
+//   searchInput.value = '';
+//   renderAllRecipes();
+// }
 
-if (categoriesList) {
-  categoriesList.addEventListener('click', handleCategoriesListClick);
-}
+// if (categoriesList) {
+//   categoriesList.addEventListener('click', handleCategoriesListClick);
+// }
 
-function handleCategoriesListClick(event) {
-  if (!event.target.classList.contains('btn')) {
-    return;
-  }
-  console.log(event.target.innerText);
-  let pickedCategory = event.target.innerText;
-  renderRecipe(pickedCategory);
-}
+// function handleCategoriesListClick(event) {
+//   if (!event.target.classList.contains('btn')) {
+//     return;
+//   }
+//   console.log(event.target.innerText);
+//   let pickedCategory = event.target.innerText;
+//   renderRecipe(pickedCategory);
+// }
 
-// =================ФУНКЦІЇ ДЛЯ ЗАПИТІВ===================
-export async function fetchAllRecipes() {
-  const response = await axios.get(`${BASE_URL}recipes?limit=${PER_PAGE}&page=${page}`);
-  return response;
-}
+// // =================ФУНКЦІЇ ДЛЯ ЗАПИТІВ===================
+// export async function fetchAllRecipes() {
+//   const response = await axios.get(`${BASE_URL}recipes?limit=${PER_PAGE}&page=${page}`);
+//   return response;
+// }
 
-async function fetchRecipeByTitle(title) {
-  const response = await axios.get(`${BASE_URL}recipes?title=${title}`);
-  return response;
-}
+// async function fetchRecipeByTitle(title) {
+//   const response = await axios.get(`${BASE_URL}recipes?title=${title}`);
+//   return response;
+// }
 
-async function fetchRecipeByCategory(category) {
-  const response = await axios.get(`${BASE_URL}recipes?category=${category}`);
-  return response;
-}
+// async function fetchRecipeByCategory(category) {
+//   const response = await axios.get(`${BASE_URL}recipes?category=${category}`);
+//   return response;
+// }
 
-// ================= ФУНКЦІЇ ДЛЯ СТВОРЕННЯ РОЗМІТКИ=================
-export function createAllRecipesMarkUp(allRecipesObj) {
-  return allRecipesObj.results
-    .map(({ title, description, preview, rating, _id }) => {
-      return `<li class="recipe-item">
-        <a href="${preview}" class="recipe-link">
-          <img src="${preview}" alt="${title}" class="recipe-image" />
-          <div class="recipe-card-bg-cover">
-            <svg class="icon-heart">
-              <path
-                d="M15.991 6.848c-2.665-3.117-7.111-3.956-10.449-1.103-3.34 2.854-3.811 7.625-1.187 11.001 2.182 2.806 8.781 8.724 10.944 10.64 0.241 0.214 0.364 0.321 0.503 0.364 0.057 0.017 0.123 0.027 0.191 0.027s0.134-0.010 0.195-0.029l-0.005 0.001c0.141-0.042 0.262-0.15 0.505-0.364 2.163-1.916 8.764-7.834 10.944-10.64 2.623-3.375 2.211-8.177-1.187-11.001-3.398-2.825-7.786-2.016-10.452 1.101z"
-              ></path>
-            </svg>
-            <div class="recipe-card-text-wrap">
-              <h3 class="recipe-title">${formatTitle(title)}</h3>
-              <p class="recipe-description">${formatDescription(description)}</p>
-              <div class="ratio-btn-wrap">
-                <div class="rating">
-                  <div class="rating-value-white">${rating}</div>
-                  <div class="rating-body">
-                    <div class="rating-active" style="width:${(rating * 100) / 5}%">
-                      <div class="rating-items">
-                        <input type="radio" class="rating-item" value="1" name="rating" />
-                        <input type="radio" class="rating-item" value="2" name="rating" />
-                        <input type="radio" class="rating-item" value="3" name="rating" />
-                        <input type="radio" class="rating-item" value="4" name="rating" />
-                        <input type="radio" class="rating-item" value="5" name="rating" />
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <button type="button" class="open-recipe-btn" data-id=${_id} data-popup="recipe">Дивитися рецепт</button>
-              </div>
-            </div>
-          </div>
-        </a>
-      </li>`;
-    })
-    .join('');
-}
+// // ================= ФУНКЦІЇ ДЛЯ СТВОРЕННЯ РОЗМІТКИ=================
+// export function createAllRecipesMarkUp(allRecipesObj) {
+//   return allRecipesObj.results
+//     .map(({ title, description, preview, rating, _id }) => {
+//       return `<li class="recipe-item">
+//         <a href="${preview}" class="recipe-link">
+//           <img src="${preview}" alt="${title}" class="recipe-image" />
+//           <div class="recipe-card-bg-cover">
+//             <svg class="icon-heart">
+//               <path
+//                 d="M15.991 6.848c-2.665-3.117-7.111-3.956-10.449-1.103-3.34 2.854-3.811 7.625-1.187 11.001 2.182 2.806 8.781 8.724 10.944 10.64 0.241 0.214 0.364 0.321 0.503 0.364 0.057 0.017 0.123 0.027 0.191 0.027s0.134-0.010 0.195-0.029l-0.005 0.001c0.141-0.042 0.262-0.15 0.505-0.364 2.163-1.916 8.764-7.834 10.944-10.64 2.623-3.375 2.211-8.177-1.187-11.001-3.398-2.825-7.786-2.016-10.452 1.101z"
+//               ></path>
+//             </svg>
+//             <div class="recipe-card-text-wrap">
+//               <h3 class="recipe-title">${formatTitle(title)}</h3>
+//               <p class="recipe-description">${formatDescription(description)}</p>
+//               <div class="ratio-btn-wrap">
+//                 <div class="rating">
+//                   <div class="rating-value-white">${rating}</div>
+//                   <div class="rating-body">
+//                     <div class="rating-active" style="width:${(rating * 100) / 5}%">
+//                       <div class="rating-items">
+//                         <input type="radio" class="rating-item" value="1" name="rating" />
+//                         <input type="radio" class="rating-item" value="2" name="rating" />
+//                         <input type="radio" class="rating-item" value="3" name="rating" />
+//                         <input type="radio" class="rating-item" value="4" name="rating" />
+//                         <input type="radio" class="rating-item" value="5" name="rating" />
+//                       </div>
+//                     </div>
+//                   </div>
+//                 </div>
+//                 <button type="button" class="open-recipe-btn" data-id=${_id} data-popup="recipe">Дивитися рецепт</button>
+//               </div>
+//             </div>
+//           </div>
+//         </a>
+//       </li>`;
+//     })
+//     .join('');
+// }
 
-// ===============ДОПОМІЖНІ ФУНКЦІЇ============== //
+// // ===============ДОПОМІЖНІ ФУНКЦІЇ============== //
 
-function formatDescription(description) {
-  let result;
-  let maxWidth = 0;
-  if (document.documentElement.clientWidth < 768) {
-    maxWidth = 98;
-  } else if (document.documentElement.clientWidth >= 768 && document.documentElement.clientWidth < 1280) {
-    maxWidth = 60;
-  } else {
-    maxWidth = 68;
-  }
+// function formatDescription(description) {
+//   let result;
+//   let maxWidth = 0;
+//   if (document.documentElement.clientWidth < 768) {
+//     maxWidth = 98;
+//   } else if (document.documentElement.clientWidth >= 768 && document.documentElement.clientWidth < 1280) {
+//     maxWidth = 60;
+//   } else {
+//     maxWidth = 68;
+//   }
 
-  result = description.length <= maxWidth ? description : description.slice(0, maxWidth) + ' ...';
+//   result = description.length <= maxWidth ? description : description.slice(0, maxWidth) + ' ...';
 
-  return result;
-}
+//   return result;
+// }
 
-function formatTitle(title) {
-  let result;
-  result = title.length <= 20 ? title : title.slice(0, 20) + ' ...';
-  return result;
-}
+// function formatTitle(title) {
+//   let result;
+//   result = title.length <= 20 ? title : title.slice(0, 20) + ' ...';
+//   return result;
+// }
 
-// ==============ФУНКЦІЇ ДЛЯ ВІДОБРАЖЕННЯ======================
-export async function renderAllRecipes() {
-  try {
-    const response = await fetchAllRecipes();
+// // ==============ФУНКЦІЇ ДЛЯ ВІДОБРАЖЕННЯ======================
+// export async function renderAllRecipes() {
+//   try {
+//     const response = await fetchAllRecipes();
 
-    if (!response.data.totalPages) {
-      Notiflix.Notify.failure('Ой! Рецепти не знайдено');
-      return;
-    }
+//     if (!response.data.totalPages) {
+//       Notiflix.Notify.failure('Ой! Рецепти не знайдено');
+//       return;
+//     }
 
-    const allRecipes = response.data;
+//     const allRecipes = response.data;
 
-    recipeList.innerHTML = createAllRecipesMarkUp(allRecipes);
-  } catch (error) {
-    console.log(error);
-    Notiflix.Notify.failure('Ой! Рецепти не знайдено');
-  }
-}
+//     recipeList.innerHTML = createAllRecipesMarkUp(allRecipes);
+//   } catch (error) {
+//     console.log(error);
+//     Notiflix.Notify.failure('Ой! Рецепти не знайдено');
+//   }
+// }
 
-async function renderSearchedRecipes(searchedTitle) {
-  try {
-    const response = await fetchRecipeByTitle(searchedTitle);
+// async function renderSearchedRecipes(searchedTitle) {
+//   try {
+//     const response = await fetchRecipeByTitle(searchedTitle);
 
-    if (!response.data.totalPages) {
-      Notiflix.Notify.failure('Ой! Рецепти не знайдено');
-      return;
-    }
+//     if (!response.data.totalPages) {
+//       Notiflix.Notify.failure('Ой! Рецепти не знайдено');
+//       return;
+//     }
 
-    const allRecipes = response.data;
+//     const allRecipes = response.data;
 
-    recipeList.innerHTML = createAllRecipesMarkUp(allRecipes);
-  } catch (error) {
-    console.log(error);
-    Notiflix.Notify.failure('Ой! Рецепти не знайдено');
-  }
-}
+//     recipeList.innerHTML = createAllRecipesMarkUp(allRecipes);
+//   } catch (error) {
+//     console.log(error);
+//     Notiflix.Notify.failure('Ой! Рецепти не знайдено');
+//   }
+// }
 
-async function renderRecipe(category) {
-  try {
-    const response = await fetchRecipeByCategory(category);
-    if (!response.data.totalPages) {
-      Notiflix.Notify.failure('Ой! Рецепти не знайдено');
-      return;
-    }
-    const pickedRecipes = response.data;
-    recipeList.innerHTML = createAllRecipesMarkUp(pickedRecipes);
-  } catch (error) {
-    console.log(error);
-    Notiflix.Notify.failure('Ой! Рецепти не знайдено');
-  }
-}
+// async function renderRecipe(category) {
+//   try {
+//     const response = await fetchRecipeByCategory(category);
+//     if (!response.data.totalPages) {
+//       Notiflix.Notify.failure('Ой! Рецепти не знайдено');
+//       return;
+//     }
+//     const pickedRecipes = response.data;
+//     recipeList.innerHTML = createAllRecipesMarkUp(pickedRecipes);
+//   } catch (error) {
+//     console.log(error);
+//     Notiflix.Notify.failure('Ой! Рецепти не знайдено');
+//   }
+// }
 //================ Bogdan >>>>>
-if (recipeList) {
-  renderAllRecipes();
-}
+// if (recipeList) {
+//   renderAllRecipes();
+// }
 const favoritesList = document.querySelector('.favorite-render-cards');
 // додаю рецепт до списку улюблених
 function addToFavorites(recipeId) {
@@ -301,7 +301,7 @@ function createRecipeMarkup(allRecipesObj) {
                     </div>
                   </div>
                 </div>
-                <button type="button" class="open-recipe-btn" data-id=${_id} data-popup="recipe">Дивитися рецепт</button>
+                <button type="button" class="open-recipe-btn" data-id=${_id} data-popup="recipe">See recipe</button>
               </div>
             </div>
           </div>
