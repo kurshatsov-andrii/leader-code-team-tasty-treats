c6072a126ae35c76659af9a3fe0f4c5f685648aa
Salnikova-Anna <anna_val@ukr.net>
2023-08-24 09:41:56 +0300

    create favorite-pagination file

--- src/js/favorites-pagination.js
+++ src/js/favorites-pagination.js
@@ -1,13 +1,33 @@
-// =============== PAGINATION=============
 import { renderFavoritesCartsListMarkup, favoritesDataInit } from '../js/favorites';
+import { createFavPagination } from './pagination';
+
+const paginationWrap2 = document.querySelector('#pagination2');
 let favoritesData = favoritesDataInit();
-export function renderFavoritesCartsListMarkupPerPage() {
+
+let PER_PAGE_FAV = 0;
+if (document.documentElement.clientWidth < 768) {
+  PER_PAGE_FAV = 9;
+} else {
+  PER_PAGE_FAV = 12;
+}
+
+let totalFavItems = 0;
+if (localStorage.favorites) {
+  favoritesData = JSON.parse(localStorage.favorites);
+  totalFavItems = favoritesData.length;
+}
+
+if (document.title === 'Favorites tasty treats' && totalFavItems > PER_PAGE_FAV) {
+  createFavPagination(totalFavItems);
+}
+
+export function renderFavoritesCartsListMarkupPerPage(page) {
   const favoritesCards = document.querySelector('.favorite-render-cards');
   if (favoritesCards) {
     favoritesCards.innerHTML = '';
     let newfavoritesData = [];
-    let startPoint = 1;
-    let endPoint = 4;
+    let startPoint = (page - 1) * PER_PAGE_FAV;
+    let endPoint = startPoint + PER_PAGE_FAV - 1;
     newfavoritesData = favoritesData.filter((recipe, index) => index >= startPoint && index <= endPoint);
     renderFavoritesCartsListMarkup(newfavoritesData);
   }
